3e7a80a29098ea7f9f54526640cb1fa4c1ceeaa8
Daniel Newman <dan@danlovesprogramming.com>
2024-05-28 20:22:19 -0500

    Changed lock script

--- roles/screensaver/files/lock.sh
+++ roles/screensaver/files/lock.sh
@@ -5,12 +5,12 @@ if [ $? -eq 0 ]; then
 	BRIGHTNESS=true
 fi
 
-if ! pidof i3lock >/dev/null; then
+if ! pidof betterlockscreen >/dev/null; then
 	if [ "$BRIGHTNESS" = true ]; then
 		brightnessctl --quiet --save
 		brightnessctl --quiet set 10%
 	fi
-	/usr/bin/i3lock --color=1d2021 --ignore-empty-password --nofork
+	betterlockscreen -l dim
 	if [ "$BRIGHTNESS" = true ]; then
 		brightnessctl --quiet --restore
 	fi
